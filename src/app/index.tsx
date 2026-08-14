0319a6bd1282c6aedf75d4e4b809f4fa575da4f2
Senta612 <parthivsenta@gmail.com>
2026-08-14 23:37:19 +0530

    Update Index component to utilize theme colors for styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,12 @@
+import { theme } from '@/theme';
 import { StyleSheet, Text, View } from "react-native";
 
-
 export default function Index() {
+  const color = theme.colors;
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>EduFlow</Text>
-      <Text style={styles.subTitle}>This is the 1408/2026 starting the app EduFlow gole is the earn a 5CR from this </Text>
+    <View style={[styles.container, { backgroundColor: color.background.screen }]}>
+      <Text style={[styles.title, { color: color.text.primary }]}>EduFlow</Text>
+      <Text style={[styles.subTitle, { color: color.text.secondary }]}>This is the 1408/2026 starting the app EduFlow gole is the earn a 5CR from this </Text>
     </View>
   )
 }
@@ -15,6 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 12,
   },
   title: {
     fontSize: 32,
@@ -23,5 +25,6 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 16,
     marginTop: 8,
+
   }
 })
